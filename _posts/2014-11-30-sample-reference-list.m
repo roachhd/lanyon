8cb73427d459db2989dd5ec847b76c53ae1b5842
KB <katie@roachhd.com>
2014-12-30 20:45:08 +1100

    Create 2014-11-30-sample-reference-list.m

---
+++ _posts/2014-11-30-sample-reference-list.m
@@ -0,0 +1,39 @@
+---
+layout: post
+title: Sample Reference List
+categories: [extras]
+---
+
+
+### APA Style Reference List
+
+American Psychological Association. (2000). *Responding therapeutically to patient expressions of sexual attraction* [DVD]. Washington, DC: Author.
+
+Andrzej, G., Michael, B., & Philip, C. (2011). High Performance Computing Clouds. In B. Benatallah (Ed.), *Cloud Computing Methodology, Systems, and Applications* (pp. 221-259): CRC Press. http://dx.doi.org/10.1201/b11149-14
+
+Apolloni, B., Pedrycz, W., Bassis, S., & Malchiodie, D. (2008). *The puzzle of granular computing*. Berlin, Germany: Springer. Retrieved from http://www.springerlink.com
+
+Australian Bureau of Statistics. (2006). *Census of population and housing: B01 selected characteristics (First release processing) postal area 6050*. Canberra, ACT: Author. Retrieved from http://www.censusdata.abs.gov.au
+
+BarackObama. (2009, July 15). Launched American Graduation Initiative to help additional 5 mill. Americans graduate college by 2020: http://bit.ly/gcTX7 [Tweet]. Retrieved from
+http://twitter.com/BarackObama/status/2651151366
+
+Brieger, B. (2005). *Lecture 3: Recruitment and involvement of trainees* [PowerPoint slides]. Retrieved from http://ocw.jhsph.edu/course.TrainingMethodsContinuingEducation/leactureNotes.cfm
+
+Briscoe, R. (in press). Egoentric apatial representation in action and perception. *Philosophy and Phenomenological Research*. Retrieved from http://coprints.org/57801/ECSRAP.F07.pdf
+
+Bulliet, R. W., Crossley, P. K., Headrick, D. R., Hirsch, S. W., Johnson, L. L., & Northrup, D. (2011). *The earth and its peoples: A global history*. Boston, MA: Wadsworth.
+
+Bureau of Infrastructure, Transport and Regional Economics. (2010). *Aircraft movements through capital city airports to 2029-30* (Report 117). Retrieved http://www.bitre.gov.au/publications/55/Files/ Report%20117.pdf
+
+British Empire 1922 and other empires. (2009). Retrieved from http://www.angelfire.com/mac/egmatthews/worldinfo/europe/empire.html
+
+Call to save indigenous languages. (2009, October 31). *The Canberra Times*, p. 7. Retrieved from http://global.factiva.com
+
+Ciccarelli, M. L. (2008). *Variation of posture and muscle activity during information and communcation technology use by adults and school children*. (Doctorial disseration). Curtin University of Technology, Bentley, W.A.
+
+cindiann. (2009, July 23). Re: Recommended photography books [Online discussion group comment]. Retrieved from http://flickr.com/groups/365libs/discuss/72157621780867836/
+
+Conking, P. W., Alley, R. B., Broecker, W. S., & Denton. (2011). *The fate of Greenland: Lessons from abrupt climate change*. Cambridge, MA: MIT Press.
+
+Curtin Uiversity Library. (2007). Autumn ivy [Image]. Retrieved from http://www.flickr.com/photos/curtinuniversitylibrary/1440410713
